3beb820acb9536eef06076e872bd64ed41afa63d
nickeatingpizza <likeeatingpizza@gmail.com>
2019-04-01 11:25:10 +0200

    fin

--- app.R
+++ app.R
@@ -1,39 +1,116 @@
 knockout_list <- readRDS("./data/knockout_list.Rds")
 oln_lookup <-  readRDS("./data/OLN_lookup.Rds")
+
 library(shiny)
-library(tidyverse)
+library(magrittr)
 library(rvest)
 library(DT)
 library(shinythemes)
 library(httr)
-# Define UI for application that draws a histogram
-ui <- fluidPage(theme = shinytheme("slate"),
-  
-   # Application title
-   titlePanel("PhosphoPEP Database"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-          selectInput(inputId = "gene_name", label = "Choose a knock-out strain from the PhosphoPEP Database",
-                     choices = knockout_list$gene 
-                     ),
-          actionButton(inputId = "button1", label = "Search"),
-          textOutput(outputId = "selected_strain")
-      ),
-      
-      mainPanel(tabsetPanel(
-        tabPanel("tab1", DT::DTOutput(outputId = "ppep_table"))
-      )
-   )
-  )
-  )
+library(dplyr)
+library(colorRamps)
+library(shinyWidgets)
 
-# Define server logic required to draw a histogram
+get_uniprot_data <- function(uniprot_ac){
+  
+  uniprot_data <- GET(paste0("https://www.uniprot.org/uniprot/", uniprot_ac, ".xml")) %>%
+    content(as = "raw", content = "text/xml") %>%
+    read_html
+  
+  protein_info <- uniprot_data %>% 
+    html_nodes(xpath = '//recommendedname/* |
+               //name[@type="primary"] | //comment[@type="function"]/text |
+               //comment[@type="interaction"]/text') %>%
+    {list(name = html_name(.), text = html_text(.))} 
+  
+  protein_info <- map2_chr(protein_info[[1]], protein_info[[2]], paste, sep = ": ")
+
+    
+  biogrid_link <- uniprot_data %>% 
+    html_nodes(xpath = '//dbreference[@type="BioGrid"] | //dbreference[@type="BioGrid"]/*') %>%
+    xml_attrs() %>%
+    unlist %>%
+    c(paste0(.[1], ": ", "<a href='https://thebiogrid.org/",
+             .["id"],
+             "' target='_blank'>ID ",
+             .["id"], "</a>",
+             "  (# interactions: ",
+             .["value"], ")")) %>% 
+    .[-(1:4)] %>% 
+    as.vector()
+  
+  
+  residues_vector <- uniprot_data %>%
+    html_nodes(xpath = '//feature[@type="modified residue"] |
+               //feature[@type="modified residue"]/location/*') %>%
+    xml_attrs() %>%
+    unlist()
+  
+  mod_aa <- tibble(
+    type = residues_vector[seq(1, to = length(residues_vector), by = 4)],
+    description = residues_vector[seq(2, to = length(residues_vector), by = 4)],
+    position = residues_vector[seq(4, to = length(residues_vector), by = 4)]
+  )
+  
+  return(list(protein_info, biogrid_link, mod_aa))
+  
+}
+
+ui <- fluidPage(theme = shinytheme("slate"),
+   
+   h1("PhosphoPEP Database"),
+   
+   sidebarLayout(
+      sidebarPanel(width = 3,
+        h4("Select a kinase knock-out strain and press Search"),
+        selectInput(inputId = "gene_name",
+                      label = NULL,
+                      choices = knockout_list$gene 
+                     ),
+        actionBttn(inputId = "button1",
+                   label = "Search",
+                   style = "simple",
+                   color = "success",
+                   size = "md",
+                   block = F),
+        div(style="padding-top:15px"),
+        tags$hr(style="border-top: dashed 1px;"),
+        h4("Get more info on the selected protein from Unirpot"),
+        actionBttn(inputId = "uniprot_btn",
+                   label = "Get Uniprot Data",
+                   style = "simple",
+                   color = "success",
+                   size = "sm",
+                   block = F),
+        div(style="padding-bottom:10px"),
+        htmlOutput(outputId = "prot_info"),
+        div(style="padding-bottom:10px"),
+        tableOutput(outputId = "residues_info"),
+        div(class='credits',
+          p("The phospho-proteomics data displayed in this website are from Phosphopep v2.0, a project by the Aebersold group at the Swiss Federal Institute of Technology (ETH) in collaboration with the Functional Genomics Center ( FGCZ ) in Zurich, Switzerland, and the Institute for Systems Biology (ISB) in Seattle, Washington USA"),
+          p('Reference:'),
+          HTML("<p style='font-style:italic'> Bernd Bodenmiller, Johan Malmstrom, Bertran Gerrits, David Campbell, Henry Lam, Alexander Schmidt, Oliver Rinner, Lukas N Mueller, Paul T Shannon, Patrick G Pedrioli, Christian Panse, Hoo-Keun Lee, Ralph Schlapbach, Ruedi Aebersold   Molecular Systems Biology. <b>2007</b> Oct; 3 (139)."),
+          tags$b(p(HTML('<a href="http://www.nature.com/msb/journal/v3/n1/full/msb4100182.html">PhosphoPep—a phosphoproteome resource for systems biology research in Drosophila Kc167 cells</a>')),
+          HTML('<div style="position:fixed;padding-bottom:15px;bottom:0;color: white;font-family: Courier;font-size: 14px;font-style: italic;text-decoration-line: underline;text-decoration-style: dashed;text-decoration-color: #28b78d;">
+            <p>this is a Shiny App coded by Nick ~circa end of 2018</p></div>')
+        )
+      )
+    ),
+  
+    mainPanel(
+      htmlOutput(outputId = "tableTitle"),
+      tabPanel("tab1", DT::DTOutput(outputId = "ppep_table"))
+      )
+    )
+)
+
+
+
 server <- function(input, output) {
   
-  output$selected_strain <- renderText({
-    paste("You are looking at knock-out strain for", input$gene_name, " from PPEP DB")
+  title <- eventReactive(input$button1, {
+    h3_text <- HTML(paste("<h3>Currently displaying data for <i>S. cerevisiae</i><b>", input$gene_name,
+                        "</b>knock-out strain from the PhosphoPEP database</h3>"))
   })
   
   df <- eventReactive(input$button1, {
@@ -49,19 +126,61 @@ server <- function(input, output) {
       filter(row_number() != 1) %>% 
       inner_join(oln_lookup, by = "OLN") %>%
       group_by(OLN, Peptide) %>%
-      mutate(GeneNames = paste(gene, collapse = ",")) %>%
+      mutate(GeneNames = paste(gene, collapse = "; "),
+             FoldChange = as.numeric(FoldChange)) %>%
       filter(row_number() == 1) %>% 
       ungroup() %>% 
-      select(-gene)# %>% 
-      #separate(GeneNames, into = paste0(rep("GeneName", 4), 1:4))
+      select(-gene, -KO_Strain) %>%
+      rename("UniprotAC" = starts_with("Swi")) %>%
+      mutate(UniprotAC = paste0("<a href='https://www.uniprot.org/uniprot/", UniprotAC,
+                          "' target='_blank'>", UniprotAC, "</a>")) %>%
+      select(GeneNames, OLN, FoldChange, Peptide, starts_with("Uni")) %>% 
+      separate(GeneNames, paste0(rep("Gene", max(str_count(.$GeneNames, ";"))+1),
+                                 1:max(str_count(.$GeneNames, ";")+1)),
+               sep = ";", fill = "right") %>% 
+      mutate_at(vars(starts_with("Gene")), funs(replace(., is.na(.), "")))
+  }) 
+
+  more_info <- eventReactive(input$uniprot_btn, {
+    get_uniprot_data(as.character(oln_lookup[oln_lookup$gene == input$gene_name, 3]))
+  })
+  
+  output$residues_info <- renderTable({
+    more_info()[[3]]
+  })
+  
+  output$prot_info <- renderText({
+     paste("<p>", unlist(more_info()[1:2]), "</p>")
+  })
+  
+  output$tableTitle <- renderText({
+    title()
   })
   
   output$ppep_table <- renderDT({
-    DT::datatable(df(), style = "bootstrap",
-                  options = list(paging = FALSE, selection = "none")) %>% 
-      formatStyle(
-        "FoldChange", color = styleInterval(0, c("red", "green"))
-      )
+   
+    brks <- c(seq(min(df()$FoldChange), to = 0, by = 1.5),
+              seq(0, max(df()$FoldChange), by = 1.5))
+    
+    length_palette <- length(brks) + 1
+    negative.length <- round(abs(range(df()$FoldChange)[1]) / 
+                               diff(range(df()$FoldChange)) * 
+                               length_palette)
+    
+    positive.length <- length_palette - negative.length
+    
+    cols <- c(colorRampPalette(c("orangered2", "white"))(negative.length),
+              colorRampPalette(c("white", "lawngreen"))(positive.length))
+    
+    DT::datatable(df(), style = "bootstrap", options = list(paging = FALSE,
+                                                            selection = "none"),
+                  escape = FALSE) %>%
+      formatStyle("FoldChange",
+                  backgroundColor = styleInterval(cuts = brks,
+                                                  values = cols),
+                  fontWeight = "bold",
+                  color = "black")
+    
   })
   
 }
@@ -70,39 +189,3 @@ server <- function(input, output) {
 # Run the application 
 shinyApp(ui = ui, server = server)
 
-
-# 
-# 
-# 
-# raw <- GET("https://www.uniprot.org/uniprot/P12345.xml") %>% 
-# 
-# parsed <- content(raw, as = "raw", content = "text/xml") %>% 
-#   read_html()
-#   
-#   parsed %>% 
-#     html_nodes(xpath = '//recommendedname/* |
-#                //name[@type="primary"] | //comment[@type="function"]/text |
-#                //comment[@type="interaction"]/text') %>% 
-#     html_text()
-# 
-#   parsed %>% 
-#     html_nodes(xpath = '//dbreference') %>% 
-#     html_table()  
-#   
-#                    
-# residues_vector <- parsed %>%
-#     html_nodes(xpath = '//feature[@type="modified residue"] |
-#                //feature[@type="modified residue"]/location/*') %>% 
-#     xml_attrs() %>%
-#     unlist() %>%
-#     tibble(
-#       type = .[seq(1, to = length(.), by = 4)]),
-#       description = residues_vector[seq(2, to = length(residues_vector), by = 4)],
-#       position = residues_vector[seq(4, to = length(residues_vector), by = 4)]
-#     )
-# 
-# %>% View
-# 
-# 
-# parsed %>% 
-# html_structure()
